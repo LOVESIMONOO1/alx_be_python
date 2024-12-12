fd051a546f13d3f106b6dd391142e2a537568e77
Simon Iwaola <138657691+LOVESIMONOO1@users.noreply.github.com>
2024-12-12 19:06:20 +0100

    add python_introduction

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,11 @@
+# define numbers
+number1 = 10
+number2 = 5
+# calculations
+sum_result = number1 + number2
+difference_result = number1 - number2
+product_result = number1 * number2
+
+print("Addition of ",number1 ,"and", number2 ,"is", sum_result)
+print("subtraction of ",number1 ,"and", number2 ,"is", difference_result)
+print("multiplication of ",number1 ,"and", number2 ,"is", product_result)
