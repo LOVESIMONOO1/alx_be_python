5f96902c36c087aa6594b60f3c7fd33cf07f3959
Simon Iwaola <138657691+LOVESIMONOO1@users.noreply.github.com>
2024-12-14 12:24:58 +0100

    Update rectangle_area.py

--- python_introduction/rectangle_area.py
+++ python_introduction/rectangle_area.py
@@ -3,7 +3,7 @@ length = 10
 width = 5
 
 #formula
-Area = length * width
+area = length * width
 
 #print result
-print("The area of the rectangle is:", Area , )
+print("The area of the rectangle is:", area , )
