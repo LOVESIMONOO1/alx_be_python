4ad466b391477c905d0e24585d13937c229a0231
Simon Iwaola <138657691+LOVESIMONOO1@users.noreply.github.com>
2024-12-22 20:48:38 +0100

    Update daily_reminder.py

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -15,4 +15,4 @@ if Time_Bound == "yes":
 else:
     reminder += ". Consider completing it when you have free time."
 # Provide a Customized Reminder
-print(f"{reminder}")    
+print(f"reminder:{reminder}")    
